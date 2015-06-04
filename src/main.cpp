ab8f6f32a72128cc196c5055477e1e49d02f0fa1
Adam Martin <adam.madram@gmail.com>
2015-06-04 16:01:19 -0500

    Moved test code to test_data.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,12 @@
 #include "os.hpp"
-#include "event-system.hpp"
-#include "event-queue.hpp"
 #include "render-system.hpp"
-#include "graphics/vertex-buffer-object.hpp"
-#include "graphics/shader.hpp"
-#include "voxelvolume.hpp"
-#include "components/transforms.hpp"
-#include "graphics/material.hpp"
-#include "entity.hpp"
-#include "components/camera.hpp"
 #include "component-update-system.hpp"
-#include <glm/gtc/matrix_transform.hpp>
-#include "resources/md5mesh.hpp"
-#include "resources/pixel-buffer.hpp"
-#include "graphics/texture-object.hpp"
+#include "components/camera.hpp"
+
+namespace tec {
+	extern void IntializeComponents();
+	extern void BuildTestEntities();
+}
 
 std::list<std::function<void(tec::frame_id_t)>> tec::ComponentUpdateSystemList::update_funcs;
 
@@ -26,82 +19,12 @@ int main(int argc, char* argv[]) {
 
 	rs.SetViewportSize(800, 600);
 
-	auto shader_files = std::list < std::pair<tec::Shader::ShaderType, std::string> > {
-		std::make_pair(tec::Shader::VERTEX, "assets/basic.vert"), std::make_pair(tec::Shader::FRAGMENT, "assets/basic.frag"),
-	};
-	auto s = tec::Shader::CreateFromFile("shader1", shader_files);
-	auto basic_fill = tec::Material::Create("material_basic", s);
-
-	shader_files = std::list < std::pair<tec::Shader::ShaderType, std::string> > {
-		std::make_pair(tec::Shader::VERTEX, "assets/basic.vert"), std::make_pair(tec::Shader::FRAGMENT, "assets/overlay.frag"),
-	};
-	auto s_overlay = tec::Shader::CreateFromFile("shader_overlay", shader_files);
-	auto overlay = tec::Material::Create("material_overlay", s_overlay);
-	overlay->SetPolygonMode(GL_LINE);
-
-	tec::ComponentUpdateSystem<tec::Position>::Initialize();
-	tec::ComponentUpdateSystem<tec::Orientation>::Initialize();
-	tec::ComponentUpdateSystem<tec::Camera>::Initialize();
-	tec::ComponentUpdateSystem<tec::Renderable>::Initialize();
-	tec::ComponentUpdateSystem<tec::View>::Initialize();
-
-	auto voxvol = tec::VoxelVolume::Create(100, "bob", 0);
-	auto voxvol_shared = voxvol.lock();
-	tec::Entity voxel1(100);
-	voxel1.Add<tec::Position>();
-	voxel1.Add<tec::Orientation>();
-
-	tec::VoxelCommand add_voxel(
-		[ ] (tec::VoxelVolume* vox_vol) {
-		vox_vol->AddVoxel(0, 1, 1);
-		vox_vol->AddVoxel(0, -1, 1);
-		vox_vol->AddVoxel(0, -1, 0);
-		vox_vol->AddVoxel(0, -1, -1);
-		vox_vol->AddVoxel(1, -1, 1);
-	});
-	tec::VoxelVolume::QueueCommand(std::move(add_voxel));
-	voxvol_shared->Update(0.0);
-	auto voxvol_vert_buffer = std::make_shared<tec::VertexBufferObject>();
-	voxel1.Add<tec::Renderable>(voxvol_vert_buffer);
-
-	tec::RenderCommand buffer_func([voxvol_vert_buffer, voxvol_shared, s] (tec::RenderSystem* sys) {
-		auto mesh = voxvol_shared->GetMesh().lock();
-		voxvol_vert_buffer->Load(mesh, s);
-		auto voxel1Renderable = tec::Entity(100).Get<tec::Renderable>().lock();
-		for (size_t i = 0; i < voxvol_vert_buffer->GetVertexGroupCount(); ++i) {
-			voxel1Renderable->vertex_groups.insert(voxvol_vert_buffer->GetVertexGroup(i));
-		}
-	});
-	tec::RenderSystem::QueueCommand(std::move(buffer_func));
-
-	std::shared_ptr<tec::MD5Mesh> mesh1 = std::make_shared<tec::MD5Mesh>();
-	mesh1->Load("assets/bob/bob.md5mesh");
-	{
-		auto renderable = std::make_shared<tec::Renderable>(std::make_shared<tec::VertexBufferObject>());
-		renderable->buffer->Load(mesh1, s);
-		for (size_t i = 0; i < renderable->buffer->GetVertexGroupCount(); ++i) {
-			renderable->vertex_groups.insert(renderable->buffer->GetVertexGroup(i));
-		}
-		tec::Entity(99).Add<tec::Renderable>(renderable);
-	}
-
-	tec::Entity camera(1);
-	camera.Add<tec::Position>();
-	camera.Add<tec::Orientation>();
-	camera.Add<tec::Camera>(1);
-	camera.Add<tec::Renderable>(voxvol_vert_buffer);
-	tec::Entity camera2(2);
-	camera2.Add<tec::Position>();
-	camera2.Add<tec::Orientation>();
-	camera2.Add<tec::Camera>(2);
-
-	tec::CameraMover cam_mover(1);
-
 	std::int64_t frame_id = 1;
 
-	auto pixbuf = tec::PixelBuffer();
-	pixbuf.Load("assets/bob/bob_head.png");
-	auto tex = tec::TextureObject(pixbuf);
+	tec::IntializeComponents();
+	tec::BuildTestEntities();
+
+	tec::CameraMover cam_mover(1);
 
 	while (!os.Closing()) {
 		tec::ComponentUpdateSystemList::UpdateAll(frame_id);
---
+++ src/test_data.cpp
@@ -0,0 +1,88 @@
+#include "graphics/vertex-buffer-object.hpp"
+#include "graphics/shader.hpp"
+#include "voxelvolume.hpp"
+#include "components/transforms.hpp"
+#include "graphics/material.hpp"
+#include "entity.hpp"
+#include "components/camera.hpp"
+#include "component-update-system.hpp"
+#include <glm/gtc/matrix_transform.hpp>
+#include "resources/md5mesh.hpp"
+#include "resources/pixel-buffer.hpp"
+#include "graphics/texture-object.hpp"
+#include "render-system.hpp"
+
+namespace tec {
+	void IntializeComponents() {
+		ComponentUpdateSystem<Position>::Initialize();
+		ComponentUpdateSystem<Orientation>::Initialize();
+		ComponentUpdateSystem<Camera>::Initialize();
+		ComponentUpdateSystem<Renderable>::Initialize();
+		ComponentUpdateSystem<View>::Initialize();
+	}
+
+	void BuildTestEntities() {
+		auto shader_files = std::list < std::pair<Shader::ShaderType, std::string> > {
+			std::make_pair(Shader::VERTEX, "assets/basic.vert"), std::make_pair(Shader::FRAGMENT, "assets/basic.frag"),
+		};
+		auto s = Shader::CreateFromFile("shader1", shader_files);
+		auto basic_fill = Material::Create("material_basic", s);
+
+		shader_files = std::list < std::pair<Shader::ShaderType, std::string> > {
+			std::make_pair(Shader::VERTEX, "assets/basic.vert"), std::make_pair(Shader::FRAGMENT, "assets/overlay.frag"),
+		};
+		auto s_overlay = Shader::CreateFromFile("shader_overlay", shader_files);
+		auto overlay = Material::Create("material_overlay", s_overlay);
+		overlay->SetPolygonMode(GL_LINE);
+
+		auto voxvol = VoxelVolume::Create(100, "bob", 0);
+		auto voxvol_shared = voxvol.lock();
+		Entity voxel1(100);
+		voxel1.Add<Position>();
+		voxel1.Add<Orientation>();
+
+		VoxelCommand add_voxel(
+			[ ] (VoxelVolume* vox_vol) {
+			vox_vol->AddVoxel(0, 1, 1);
+			vox_vol->AddVoxel(0, -1, 1);
+			vox_vol->AddVoxel(0, -1, 0);
+			vox_vol->AddVoxel(0, -1, -1);
+			vox_vol->AddVoxel(1, -1, 1);
+		});
+		VoxelVolume::QueueCommand(std::move(add_voxel));
+		voxvol_shared->Update(0.0);
+		auto voxvol_vert_buffer = std::make_shared<VertexBufferObject>();
+		voxel1.Add<Renderable>(voxvol_vert_buffer);
+
+		RenderCommand buffer_func([voxvol_vert_buffer, voxvol_shared, s] (RenderSystem* sys) {
+			auto mesh = voxvol_shared->GetMesh().lock();
+			voxvol_vert_buffer->Load(mesh, s);
+			auto voxel1Renderable = Entity(100).Get<Renderable>().lock();
+			for (size_t i = 0; i < voxvol_vert_buffer->GetVertexGroupCount(); ++i) {
+				voxel1Renderable->vertex_groups.insert(voxvol_vert_buffer->GetVertexGroup(i));
+			}
+		});
+		RenderSystem::QueueCommand(std::move(buffer_func));
+
+		std::shared_ptr<MD5Mesh> mesh1 = std::make_shared<MD5Mesh>();
+		mesh1->Load("assets/bob/bob.md5mesh");
+		{
+			auto renderable = std::make_shared<Renderable>(std::make_shared<VertexBufferObject>());
+			renderable->buffer->Load(mesh1, s);
+			for (size_t i = 0; i < renderable->buffer->GetVertexGroupCount(); ++i) {
+				renderable->vertex_groups.insert(renderable->buffer->GetVertexGroup(i));
+			}
+			Entity(99).Add<Renderable>(renderable);
+		}
+
+		Entity camera(1);
+		camera.Add<Position>();
+		camera.Add<Orientation>();
+		camera.Add<Camera>(1);
+		camera.Add<Renderable>(voxvol_vert_buffer);
+		Entity camera2(2);
+		camera2.Add<Position>();
+		camera2.Add<Orientation>();
+		camera2.Add<Camera>(2);
+	}
+}
