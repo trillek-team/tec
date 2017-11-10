3fabfad0d2f99ba4b23cce78b58a923bc4503652
Devin Vaukz <devin.vaukz@gmail.com>
2017-11-09 20:13:43 -0800

    Require Core Profile on macOS

--- client/os.cpp
+++ client/os.cpp
@@ -47,9 +47,15 @@ namespace tec {
 
 		// Create a windowed mode window and its OpenGL context.
 		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, glMajor);
-		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, glMinor);
-		//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // Only with GL 3.2
+    #ifdef __APPLE__
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // Apple requires Core for modern GL
+        // Ignore the requested version.  macOS needs 3.3
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    #else
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, glMajor);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, glMinor);
+    #endif
 		this->window = glfwCreateWindow(width, height, title.c_str(), NULL, NULL);
 
 		if (!this->window) {
@@ -62,10 +68,12 @@ namespace tec {
 		// attach the context
 		glfwMakeContextCurrent(this->window);
 
+    #ifndef __APPLE__
 		if (glewInit() != GLEW_OK) {
 			l->critical("[OS] Can initialize glew");
 			return false;
 		}
+    #endif
 
 		// check the context version
 		std::string glcx_version((char*)glGetString(GL_VERSION));
