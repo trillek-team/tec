a1a5e5651c2df0bd7b260f77e131d57cff343cc6
Adam Martin <adam.madram@gmail.com>
2019-11-10 11:54:06 -0600

    CMake CXX_FLAGS had been the issue the whole time

--- client/resources/vorbis-stream.hpp
+++ client/resources/vorbis-stream.hpp
@@ -12,29 +12,34 @@
 #include "filesystem.hpp"
 
 #define STB_VORBIS_HEADER_ONLY
+
 // Rather than modifying the source to have these pragma just wrap the usages in the trillek code
-#pragma warning( push )
-#pragma warning( disable : 4244) // Conversion loss of data
+#if defined(_MSC_VER)
+	#pragma warning( push )
+	#pragma warning( disable : 4244) // Conversion loss of data
 
-#pragma warning( push )
-#pragma warning( disable : 4457) // declaration hides function parameter
+	#pragma warning( push )
+	#pragma warning( disable : 4457) // declaration hides function parameter
 
-#pragma warning( push )
-#pragma warning( disable : 4456) // declaration hides previous declaration
+	#pragma warning( push )
+	#pragma warning( disable : 4456) // declaration hides previous declaration
 
-#pragma warning( push )
-#pragma warning( disable : 4245) // signed / unsigned mismatch
+	#pragma warning( push )
+	#pragma warning( disable : 4245) // signed / unsigned mismatch
 
-#pragma warning( push )
-#pragma warning( disable : 4701) // potentially uninitialized variable
+	#pragma warning( push )
+	#pragma warning( disable : 4701) // potentially uninitialized variable
 
-#include "resources/stb_vorbis.c"
+	#include "resources/stb_vorbis.c"
 
-#pragma warning( pop ) 
-#pragma warning( pop ) 
-#pragma warning( pop ) 
-#pragma warning( pop ) 
-#pragma warning( pop ) 
+	#pragma warning( pop ) 
+	#pragma warning( pop ) 
+	#pragma warning( pop ) 
+	#pragma warning( pop ) 
+	#pragma warning( pop )
+#else
+	#include "resources/stb_vorbis.c"
+#endif
 
 namespace tec {
 	class VorbisStream {
