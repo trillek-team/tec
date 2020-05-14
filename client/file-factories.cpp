cfa14edc4a629dbfd8f436b99020edd2b233085c
Adam Martin <adam.madram@gmail.com>
2020-05-13 23:23:33 -0500

    Add missing file

---
+++ client/file-factories.cpp
@@ -0,0 +1,38 @@
+// Copyright (c) 2013-2016 Trillek contributors. See AUTHORS.txt for details
+// Licensed under the terms of the LGPLv3. See licenses/lgpl-3.0.txt
+
+#include <functional>
+#include <unordered_map>
+
+#include "resources/md5mesh.hpp"
+#include "resources/obj.hpp"
+#include "resources/md5anim.hpp"
+#include "resources/vorbis-stream.hpp"
+#include "resources/script-file.hpp"
+
+#include "types.hpp"
+#include "filesystem.hpp"
+
+namespace tec {
+	std::unordered_map<std::string, std::function<void(std::string)>> file_factories;
+	template <typename T>
+	void AddFileFactory() {
+		file_factories[GetTypeEXT<T>()] = [] (std::string fname) {
+			FilePath path(fname);
+			if (path.isAbsolutePath()) {
+				T::Create(fname);
+			}
+			else {
+				T::Create(FilePath::GetAssetPath(fname));
+			}
+		};
+	}
+
+	void InitializeFileFactories() {
+		AddFileFactory<MD5Mesh>();
+		AddFileFactory<MD5Anim>();
+		AddFileFactory<OBJ>();
+		AddFileFactory<VorbisStream>();
+		AddFileFactory<ScriptFile>();
+	}
+}
