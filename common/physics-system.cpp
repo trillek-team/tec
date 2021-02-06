5d83d0dc3d84026df9e8f4b2e93a93165e35385f
dimaguy <afonsoantonio2003@gmail.com>
2021-02-06 23:35:46 +0000

    Removing unneeded license headers (#175)
    * Removing unneeded license headers

--- common/physics-system.cpp
+++ common/physics-system.cpp
@@ -1,6 +1,3 @@
-// Copyright (c) 2013-2016 Trillek contributors. See AUTHORS.txt for details
-// Licensed under the terms of the LGPLv3. See licenses/lgpl-3.0.txt
-
 #include "physics-system.hpp"
 
 // #include "physics/physics-debug-drawer.hpp"
--- tests/filesystem_test.cpp
+++ tests/filesystem_test.cpp
@@ -1,6 +1,3 @@
-// Copyright (c) 2013-2016 Trillek contributors. See AUTHORS.txt for details
-// Licensed under the terms of the LGPLv3. See licenses/lgpl-3.0.txt
-
 /**
 * Unit tests of TEC - FileSystem
 */
