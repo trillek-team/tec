f6cb4f831e94d2acc6d04517b61b374df508ef75
Adam Martin <adam.madram@gmail.com>
2022-08-02 14:58:46 -0500

    Cleanup based on feedback

--- cmake/helpers.cmake
+++ cmake/helpers.cmake
@@ -1,13 +1,13 @@
-function(set_warning_flag)
-	cmake_parse_arguments("SET_WARNING_FLAG" "" "TARGET" "" ${ARGN})
-	if (MSVC)
-		set(WARNING_FLAG /W4)
-	else()
-		set(WARNING_FLAG -Wall)
-	endif()
-	if (TARGET ${SET_WARNING_FLAG_TARGET})
-		set_property(TARGET ${SET_WARNING_FLAG_TARGET} APPEND_STRING PROPERTY COMPILE_FLAGS ${WARNING_FLAG})
-	endif()
+function(setup_compiler)
+	set(isMSVC "$<CXX_COMPILER_ID:MSVC>")
+	set(isWin32 "$<PLATFORM_ID:Windows>")
+	set(isCLANG "$<CXX_COMPILER_ID:Clang>")
+
+	set(msvcFlags "/W4 /WX /wd4125 /experimental:external /external:anglebrackets /external:W0 ")
+	add_compile_options("$<${isMSVC}: $<${isCLANG}:/EHsc> ${msvcFlags}, -Wall>")
+
+	set(msvcDefines "PROTOBUF_USE_DLLS _CRT_SECURE_NO_WARNINGS _SILENCE_ALL_CXX17_DEPRECATION_WARNINGS _WIN32_WINNT=0x0601 WIN32_LEAN_AND_MEAN")
+	add_compile_definitions("$<${isMSVC}: ${msvcDefines}> $<${isWin32}: WIN32>")
 endfunction()
 
 # Used by IDEs to nicely view header files
@@ -31,7 +31,6 @@ function(add_program)
 	cmake_parse_arguments("ADD_PROGRAM" "" "TARGET" "FILE_LIST;LINK_LIBS" ${ARGN})
 
 	add_executable(${ADD_PROGRAM_TARGET} ${ADD_PROGRAM_FILE_LIST})
-	set_warning_flag(TARGET ${ADD_PROGRAM_TARGET})
 	
 	target_include_directories(${ADD_PROGRAM_TARGET} PRIVATE ${CMAKE_CURRENT_SOURCE_DIR})
 	target_link_libraries(${ADD_PROGRAM_TARGET} PUBLIC ${trillek-common_LIBRARY_NAME} ${ADD_PROGRAM_LINK_LIBS})
@@ -51,5 +50,5 @@ function(add_interface_lib)
 		${ADD_INTERFACE_LIB_FILE_LIST}
 		PUBLIC FILE_SET HEADERS FILES ${CURRENT_SOURCE_DIR_HEADERS}
 	)
-	target_compile_features(${ADD_INTERFACE_LIB_TARGET} PUBLIC cxx_std_17)
+	target_compile_features(${ADD_INTERFACE_LIB_TARGET} INTERFACE cxx_std_17)
 endfunction()
