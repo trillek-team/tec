baecd5cd40ccdf684f5de3c2f2ef52577c08d283
Adam Martin <adam.madram@gmail.com>
2024-12-26 16:16:04 -0600

    Update configurations, key mappings, and C++ standard
    - Updated `c_cpp_properties.json` with new "Win32" configuration and set `mergeConfigurations` to true.
    - Modified `settings.json` to add new C/C++ settings for case sensitivity, error squiggles, and default standards.
    - Added new hidden presets for Ninja Multi-Config with MSVC and Clang compilers in `CMakePresets.json`.
    - Refactored `imgui-system.cpp`:
      - Initialized `inifilename` and `logfilename` with local constants.
      - Removed keyboard mapping setup.
      - Added `GlfwKeyToImGuiKey` function for key code mapping.
      - Updated `KeyboardEvent::On` function for robust key event handling.
      - Used `SetTexID` in `CreateDeviceObjects` and restored last bound texture.
    - Changed C++ standard to C++20 in `helpers.cmake` for `add_program` and `add_interface_lib`.
    - Updated `vcpkg.json` to new `builtin-baseline` commit hash.

--- client/imgui-system.cpp
+++ client/imgui-system.cpp
@@ -23,39 +23,17 @@ std::size_t IMGUISystem::vbo_size = 0, IMGUISystem::ibo_size = 0;
 unsigned int IMGUISystem::vbo = 0, IMGUISystem::ibo = 0, IMGUISystem::vao = 0;
 GLuint IMGUISystem::font_texture = 0;
 
-std::string inifilename;
-std::string logfilename;
-
 IMGUISystem::IMGUISystem() { ImGui::CreateContext(); }
 
 void IMGUISystem::Initialize(GLFWwindow* _window) {
 	IMGUISystem::window = _window;
 	auto& io = ImGui::GetIO();
-	inifilename = (Path::GetUserSettingsPath() / "imgui.ini").toString();
-	logfilename = (Path::GetUserCachePath() / "imgui_log.txt").toString();
-	io.IniFilename = inifilename.c_str();
+	const std::string ini_filename = (Path::GetUserSettingsPath() / "imgui.ini").toString();
+	const std::string log_filename = (Path::GetUserCachePath() / "imgui_log.txt").toString();
+	io.IniFilename = ini_filename.c_str();
 #if defined(DEBUG) || defined(_DEBUG)
-	io.LogFilename = logfilename.c_str();
+	io.LogFilename = log_filename.c_str();
 #endif
-
-	// Keyboard mapping. ImGui will use those indices to peek into the io.KeyDown[] array.
-	io.KeyMap[ImGuiKey_Tab] = GLFW_KEY_TAB;
-	io.KeyMap[ImGuiKey_LeftArrow] = GLFW_KEY_LEFT;
-	io.KeyMap[ImGuiKey_RightArrow] = GLFW_KEY_RIGHT;
-	io.KeyMap[ImGuiKey_UpArrow] = GLFW_KEY_UP;
-	io.KeyMap[ImGuiKey_DownArrow] = GLFW_KEY_DOWN;
-	io.KeyMap[ImGuiKey_Home] = GLFW_KEY_HOME;
-	io.KeyMap[ImGuiKey_End] = GLFW_KEY_END;
-	io.KeyMap[ImGuiKey_Delete] = GLFW_KEY_DELETE;
-	io.KeyMap[ImGuiKey_Backspace] = GLFW_KEY_BACKSPACE;
-	io.KeyMap[ImGuiKey_Enter] = GLFW_KEY_ENTER;
-	io.KeyMap[ImGuiKey_Escape] = GLFW_KEY_ESCAPE;
-	io.KeyMap[ImGuiKey_A] = GLFW_KEY_A;
-	io.KeyMap[ImGuiKey_C] = GLFW_KEY_C;
-	io.KeyMap[ImGuiKey_V] = GLFW_KEY_V;
-	io.KeyMap[ImGuiKey_X] = GLFW_KEY_X;
-	io.KeyMap[ImGuiKey_Y] = GLFW_KEY_Y;
-	io.KeyMap[ImGuiKey_Z] = GLFW_KEY_Z;
 
 	io.SetClipboardTextFn = SetClipboardText;
 	io.GetClipboardTextFn = GetClipboardText;
@@ -242,7 +220,7 @@ void IMGUISystem::CreateDeviceObjects() {
 	glEnableVertexAttribArray(IMGUISystem::uv_attribute_location);
 	glEnableVertexAttribArray(IMGUISystem::color_attribute_location);
 
-#define OFFSETOF(TYPE, ELEMENT) ((std::size_t)&(((TYPE*)0)->ELEMENT))
+#define OFFSETOF(TYPE, ELEMENT) ((std::size_t) & (((TYPE*)0)->ELEMENT))
 	glVertexAttribPointer(
 			IMGUISystem::position_attribute_location,
 			2,
@@ -272,22 +250,29 @@ void IMGUISystem::CreateDeviceObjects() {
 	// Create the font texture.
 	unsigned char* pixels;
 	int width, height;
-	auto& io = ImGui::GetIO();
+	const auto& io = ImGui::GetIO();
 	io.Fonts->GetTexDataAsRGBA32(&pixels, &width, &height); // Load as RGBA 32-bits for OpenGL3 demo because it is more
 			// likely to be compatible with user's existing shader.
+
+	GLint last_texture;
+	glGetIntegerv(GL_TEXTURE_BINDING_2D, &last_texture);
 
 	glGenTextures(1, &font_texture);
 	glBindTexture(GL_TEXTURE_2D, font_texture);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
 
 	// Store our identifier
-	io.Fonts->TexID = (void*)(intptr_t)font_texture;
+	io.Fonts->SetTexID((intptr_t)font_texture);
 
 	// Cleanup (don't clear the input data if you want to append new fonts later)
 	io.Fonts->ClearInputData();
 	io.Fonts->ClearTexData();
+
+	glBindTexture(GL_TEXTURE_2D, last_texture);
 }
 
 const char* IMGUISystem::GetClipboardText(void*) { return glfwGetClipboardString(IMGUISystem::window); }
@@ -422,25 +407,158 @@ void IMGUISystem::On(eid, std::shared_ptr<MouseBtnEvent> data) {
 	}
 }
 
-void IMGUISystem::On(eid, std::shared_ptr<KeyboardEvent> data) {
+static ImGuiKey GlfwKeyToImGuiKey(int keycode) {
+	switch (keycode) {
+	case GLFW_KEY_TAB: return ImGuiKey_Tab;
+	case GLFW_KEY_LEFT: return ImGuiKey_LeftArrow;
+	case GLFW_KEY_RIGHT: return ImGuiKey_RightArrow;
+	case GLFW_KEY_UP: return ImGuiKey_UpArrow;
+	case GLFW_KEY_DOWN: return ImGuiKey_DownArrow;
+	case GLFW_KEY_PAGE_UP: return ImGuiKey_PageUp;
+	case GLFW_KEY_PAGE_DOWN: return ImGuiKey_PageDown;
+	case GLFW_KEY_HOME: return ImGuiKey_Home;
+	case GLFW_KEY_END: return ImGuiKey_End;
+	case GLFW_KEY_INSERT: return ImGuiKey_Insert;
+	case GLFW_KEY_DELETE: return ImGuiKey_Delete;
+	case GLFW_KEY_BACKSPACE: return ImGuiKey_Backspace;
+	case GLFW_KEY_SPACE: return ImGuiKey_Space;
+	case GLFW_KEY_ENTER: return ImGuiKey_Enter;
+	case GLFW_KEY_ESCAPE: return ImGuiKey_Escape;
+	case GLFW_KEY_APOSTROPHE: return ImGuiKey_Apostrophe;
+	case GLFW_KEY_COMMA: return ImGuiKey_Comma;
+	case GLFW_KEY_MINUS: return ImGuiKey_Minus;
+	case GLFW_KEY_PERIOD: return ImGuiKey_Period;
+	case GLFW_KEY_SLASH: return ImGuiKey_Slash;
+	case GLFW_KEY_SEMICOLON: return ImGuiKey_Semicolon;
+	case GLFW_KEY_EQUAL: return ImGuiKey_Equal;
+	case GLFW_KEY_LEFT_BRACKET: return ImGuiKey_LeftBracket;
+	case GLFW_KEY_BACKSLASH: return ImGuiKey_Backslash;
+	case GLFW_KEY_RIGHT_BRACKET: return ImGuiKey_RightBracket;
+	case GLFW_KEY_GRAVE_ACCENT: return ImGuiKey_GraveAccent;
+	case GLFW_KEY_CAPS_LOCK: return ImGuiKey_CapsLock;
+	case GLFW_KEY_SCROLL_LOCK: return ImGuiKey_ScrollLock;
+	case GLFW_KEY_NUM_LOCK: return ImGuiKey_NumLock;
+	case GLFW_KEY_PRINT_SCREEN: return ImGuiKey_PrintScreen;
+	case GLFW_KEY_PAUSE: return ImGuiKey_Pause;
+	case GLFW_KEY_KP_0: return ImGuiKey_Keypad0;
+	case GLFW_KEY_KP_1: return ImGuiKey_Keypad1;
+	case GLFW_KEY_KP_2: return ImGuiKey_Keypad2;
+	case GLFW_KEY_KP_3: return ImGuiKey_Keypad3;
+	case GLFW_KEY_KP_4: return ImGuiKey_Keypad4;
+	case GLFW_KEY_KP_5: return ImGuiKey_Keypad5;
+	case GLFW_KEY_KP_6: return ImGuiKey_Keypad6;
+	case GLFW_KEY_KP_7: return ImGuiKey_Keypad7;
+	case GLFW_KEY_KP_8: return ImGuiKey_Keypad8;
+	case GLFW_KEY_KP_9: return ImGuiKey_Keypad9;
+	case GLFW_KEY_KP_DECIMAL: return ImGuiKey_KeypadDecimal;
+	case GLFW_KEY_KP_DIVIDE: return ImGuiKey_KeypadDivide;
+	case GLFW_KEY_KP_MULTIPLY: return ImGuiKey_KeypadMultiply;
+	case GLFW_KEY_KP_SUBTRACT: return ImGuiKey_KeypadSubtract;
+	case GLFW_KEY_KP_ADD: return ImGuiKey_KeypadAdd;
+	case GLFW_KEY_KP_ENTER: return ImGuiKey_KeypadEnter;
+	case GLFW_KEY_KP_EQUAL: return ImGuiKey_KeypadEqual;
+	case GLFW_KEY_LEFT_SHIFT: return ImGuiKey_LeftShift;
+	case GLFW_KEY_LEFT_CONTROL: return ImGuiKey_LeftCtrl;
+	case GLFW_KEY_LEFT_ALT: return ImGuiKey_LeftAlt;
+	case GLFW_KEY_LEFT_SUPER: return ImGuiKey_LeftSuper;
+	case GLFW_KEY_RIGHT_SHIFT: return ImGuiKey_RightShift;
+	case GLFW_KEY_RIGHT_CONTROL: return ImGuiKey_RightCtrl;
+	case GLFW_KEY_RIGHT_ALT: return ImGuiKey_RightAlt;
+	case GLFW_KEY_RIGHT_SUPER: return ImGuiKey_RightSuper;
+	case GLFW_KEY_MENU: return ImGuiKey_Menu;
+	case GLFW_KEY_0: return ImGuiKey_0;
+	case GLFW_KEY_1: return ImGuiKey_1;
+	case GLFW_KEY_2: return ImGuiKey_2;
+	case GLFW_KEY_3: return ImGuiKey_3;
+	case GLFW_KEY_4: return ImGuiKey_4;
+	case GLFW_KEY_5: return ImGuiKey_5;
+	case GLFW_KEY_6: return ImGuiKey_6;
+	case GLFW_KEY_7: return ImGuiKey_7;
+	case GLFW_KEY_8: return ImGuiKey_8;
+	case GLFW_KEY_9: return ImGuiKey_9;
+	case GLFW_KEY_A: return ImGuiKey_A;
+	case GLFW_KEY_B: return ImGuiKey_B;
+	case GLFW_KEY_C: return ImGuiKey_C;
+	case GLFW_KEY_D: return ImGuiKey_D;
+	case GLFW_KEY_E: return ImGuiKey_E;
+	case GLFW_KEY_F: return ImGuiKey_F;
+	case GLFW_KEY_G: return ImGuiKey_G;
+	case GLFW_KEY_H: return ImGuiKey_H;
+	case GLFW_KEY_I: return ImGuiKey_I;
+	case GLFW_KEY_J: return ImGuiKey_J;
+	case GLFW_KEY_K: return ImGuiKey_K;
+	case GLFW_KEY_L: return ImGuiKey_L;
+	case GLFW_KEY_M: return ImGuiKey_M;
+	case GLFW_KEY_N: return ImGuiKey_N;
+	case GLFW_KEY_O: return ImGuiKey_O;
+	case GLFW_KEY_P: return ImGuiKey_P;
+	case GLFW_KEY_Q: return ImGuiKey_Q;
+	case GLFW_KEY_R: return ImGuiKey_R;
+	case GLFW_KEY_S: return ImGuiKey_S;
+	case GLFW_KEY_T: return ImGuiKey_T;
+	case GLFW_KEY_U: return ImGuiKey_U;
+	case GLFW_KEY_V: return ImGuiKey_V;
+	case GLFW_KEY_W: return ImGuiKey_W;
+	case GLFW_KEY_X: return ImGuiKey_X;
+	case GLFW_KEY_Y: return ImGuiKey_Y;
+	case GLFW_KEY_Z: return ImGuiKey_Z;
+	case GLFW_KEY_F1: return ImGuiKey_F1;
+	case GLFW_KEY_F2: return ImGuiKey_F2;
+	case GLFW_KEY_F3: return ImGuiKey_F3;
+	case GLFW_KEY_F4: return ImGuiKey_F4;
+	case GLFW_KEY_F5: return ImGuiKey_F5;
+	case GLFW_KEY_F6: return ImGuiKey_F6;
+	case GLFW_KEY_F7: return ImGuiKey_F7;
+	case GLFW_KEY_F8: return ImGuiKey_F8;
+	case GLFW_KEY_F9: return ImGuiKey_F9;
+	case GLFW_KEY_F10: return ImGuiKey_F10;
+	case GLFW_KEY_F11: return ImGuiKey_F11;
+	case GLFW_KEY_F12: return ImGuiKey_F12;
+	case GLFW_KEY_F13: return ImGuiKey_F13;
+	case GLFW_KEY_F14: return ImGuiKey_F14;
+	case GLFW_KEY_F15: return ImGuiKey_F15;
+	case GLFW_KEY_F16: return ImGuiKey_F16;
+	case GLFW_KEY_F17: return ImGuiKey_F17;
+	case GLFW_KEY_F18: return ImGuiKey_F18;
+	case GLFW_KEY_F19: return ImGuiKey_F19;
+	case GLFW_KEY_F20: return ImGuiKey_F20;
+	case GLFW_KEY_F21: return ImGuiKey_F21;
+	case GLFW_KEY_F22: return ImGuiKey_F22;
+	case GLFW_KEY_F23: return ImGuiKey_F23;
+	case GLFW_KEY_F24: return ImGuiKey_F24;
+	default: return ImGuiKey_None;
+	}
+}
+
+void IMGUISystem::On(eid, const std::shared_ptr<KeyboardEvent> data) {
 	auto& io = ImGui::GetIO();
-	if (data->action == KeyboardEvent::KEY_DOWN) {
-		io.KeysDown[data->key] = true;
-	}
-	else if (data->action == KeyboardEvent::KEY_UP) {
-		io.KeysDown[data->key] = false;
-	}
-
-	if (data->action == KeyboardEvent::KEY_CHAR) {
+	const auto imgui_key = GlfwKeyToImGuiKey(data->key);
+	switch (data->action) {
+	case KeyboardEvent::KEY_DOWN: io.AddKeyEvent(imgui_key, true); break;
+	case KeyboardEvent::KEY_UP: io.AddKeyEvent(imgui_key, false); break;
+	case KeyboardEvent::KEY_CHAR:
 		if (data->key > 0 && data->key < 0x10000) {
-			io.AddInputCharacter((unsigned short)data->key);
+			io.AddInputCharacter(static_cast<unsigned short>(data->key));
 		}
-		return;
+		break;
+	case KeyboardEvent::KEY_REPEAT: break;
 	}
 
-	(void)data->mods; // Modifiers are not reliable across systems
-	io.KeyCtrl = io.KeysDown[GLFW_KEY_LEFT_CONTROL] || io.KeysDown[GLFW_KEY_RIGHT_CONTROL];
-	io.KeyShift = io.KeysDown[GLFW_KEY_LEFT_SHIFT] || io.KeysDown[GLFW_KEY_RIGHT_SHIFT];
-	io.KeyAlt = io.KeysDown[GLFW_KEY_LEFT_ALT] || io.KeysDown[GLFW_KEY_RIGHT_ALT];
+	io.AddKeyEvent(
+			ImGuiMod_Ctrl,
+			(glfwGetKey(window, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS)
+					|| (glfwGetKey(window, GLFW_KEY_RIGHT_CONTROL) == GLFW_PRESS));
+	io.AddKeyEvent(
+			ImGuiMod_Shift,
+			(glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS)
+					|| (glfwGetKey(window, GLFW_KEY_RIGHT_SHIFT) == GLFW_PRESS));
+	io.AddKeyEvent(
+			ImGuiMod_Alt,
+			(glfwGetKey(window, GLFW_KEY_LEFT_ALT) == GLFW_PRESS)
+					|| (glfwGetKey(window, GLFW_KEY_RIGHT_ALT) == GLFW_PRESS));
+	io.AddKeyEvent(
+			ImGuiMod_Super,
+			(glfwGetKey(window, GLFW_KEY_LEFT_SUPER) == GLFW_PRESS)
+					|| (glfwGetKey(window, GLFW_KEY_RIGHT_SUPER) == GLFW_PRESS));
 }
 } // end namespace tec
